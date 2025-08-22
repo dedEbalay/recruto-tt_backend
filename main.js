e46164b18b9feab661aae88172d9782310a0c5b4
dedEbalay <lonshakov.max@gmail.com>
2025-08-22 09:40:57 +0300

    create

---
+++ main.js
@@ -0,0 +1,15 @@
+import exporess from 'express'
+
+const PORT = '80'
+
+const app = exporess()
+
+app.get('/', (req, res) => {
+    console.log(req.query);
+    
+    res.status(200).json(`Hello ${req.query.name}! ${req.query.message}`)
+})
+
+app.listen(PORT, () => {
+    
+})
